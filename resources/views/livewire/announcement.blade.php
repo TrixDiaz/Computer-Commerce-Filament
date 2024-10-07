6e7ede79ee4a30a13ada63cff05bd4e13aaaf06d
XirTech <john.darlucio022@gmail.com>
2024-10-08 03:05:23 +0800

    landing page

---
+++ resources/views/livewire/announcement.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <div class="dark:bg-slate-800 dark:text-white">
+        <div class="max-w-7xl mx-auto">
+            <div class="hidden md:block overflow-hidden py-2">
+                <div class="relative whitespace-nowrap">
+                    <div class="animate-marquee inline-block">
+                        <span class="uppercase text-md font-poppins">
+                            Description • Amazing Products • Great Deals • Shop Now •&nbsp;
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

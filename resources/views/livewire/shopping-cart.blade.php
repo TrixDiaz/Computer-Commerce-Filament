346eaec27470322167ddc001e125729d286d05fb
XirTech <john.darlucio022@gmail.com>
2024-10-11 15:41:50 +0800

    default value in images

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -79,7 +79,16 @@
                             @forelse($relatedProducts as $product)
                                 <div class="space-y-6 overflow-hidden rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                                     <a href="#" class="overflow-hidden rounded">
-                                        <img class="mx-auto h-44 w-44" src="{{ $product['images'] }}" alt="{{ $product['name'] }}" />
+                                        <img class="mx-auto h-44 w-44 dark:hidden" 
+                                             src="{{ $product['images'] ?? '/images/laptop-image.png' }}" 
+                                             alt="{{ $product['name'] }}"
+                                             onmouseover="this.src='/images/hover-image.png'"
+                                             onmouseout="this.src='{{ $product['images'] ?? '/images/laptop-image.png' }}'" />
+                                        <img class="mx-auto h-44 w-44 hidden dark:block" 
+                                             src="{{ $product['images'] ?? '/images/laptop-image.png' }}" 
+                                             alt="{{ $product['name'] }}"
+                                             onmouseover="this.src='/images/hover-image.png'"
+                                             onmouseout="this.src='{{ $product['images'] ?? '/images/laptop-image.png' }}'" />
                                     </a>
                                     <div>
                                         <a href="#" class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{{ $product['name'] }}</a>

4b3dd20042474368ab03c1c3b5f51d35c8f9b504
XirTech <john.darlucio022@gmail.com>
2024-10-14 01:47:15 +0800

    update on landing page

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -4,7 +4,7 @@
             <div class="content-center justify-self-start md:col-span-7 md:text-start">
                 <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight dark:text-white md:max-w-2xl md:text-5xl xl:text-6xl">Limited Time Offer!<br />Up to 50% OFF!</h1>
                 <p class="mb-4 max-w-2xl text-gray-500 dark:text-gray-400 md:mb-12 md:text-lg mb-3 lg:mb-5 lg:text-xl">Don't Wait - Limited Stock at Unbeatable Prices!</p>
-                <a href="#" class="inline-block rounded-lg bg-blue-700 px-6 py-3.5 text-white text-center font-medium hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Shop Now</a>
+                <a href="{{ route('catalog') }}" class="inline-block rounded-lg bg-blue-700 px-6 py-3.5 text-white text-center font-medium hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Shop Now</a>
             </div>
             <div class="hidden md:col-span-5 md:mt-0 md:flex">
                 <img class="dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/girl-shopping-list.svg" alt="shopping illustration" />

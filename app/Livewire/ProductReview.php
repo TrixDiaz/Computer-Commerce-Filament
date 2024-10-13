0865ae45859f3cca7a4d4506448da2c5337caa8e
XirTech <john.darlucio022@gmail.com>
2024-10-13 12:47:49 +0800

    review

--- app/Livewire/ProductReview.php
+++ app/Livewire/ProductReview.php
@@ -3,9 +3,30 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Product;
+use App\Models\Review;
 
 class ProductReview extends Component
 {
+    public $slug;
+    public $reviews;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+        $this->loadReviews();
+    }
+
+    public function loadReviews()
+    {
+        $product = Product::where('slug', $this->slug)->firstOrFail();
+        $this->reviews = $product->reviews()
+            ->with('user')
+            ->where('is_approved', 1)
+            ->latest()
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.product-review');

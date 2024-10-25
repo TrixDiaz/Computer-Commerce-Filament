de863c4e704cc87a8a1b92104169425c028e2451
XirTech <john.darlucio022@gmail.com>
2024-10-25 10:45:19 +0800

    adjust file size

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -97,6 +97,7 @@ class ProductResource extends Resource
                             ->label('3D Model')
                             ->disk('public')
                             ->directory('models')
+                            ->maxSize(50 * 1024), // 50 MB
                     ]),
 
                     Forms\Components\Section::make()->schema([
